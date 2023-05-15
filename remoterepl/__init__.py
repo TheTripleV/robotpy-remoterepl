da53abf4ca8eee042e256cc5f1a1a8f4bb89a60f
Vasista Vovveti <vasistavovveti@gmail.com>
2023-05-15 14:34:03 -0500

    Update __init__.py

--- remoterepl/__init__.py
+++ remoterepl/__init__.py
@@ -1,34 +1,41 @@
-import networktables
+import ntcore
 import code
 import io
 import time
 from contextlib import redirect_stdout, redirect_stderr
 
-class RemoteREPL:
+# NOTE: The args for pynetworktables and _pyntcore for nt notifiers isn't the same so I got rid of it.
 
-    def __init__(self, robot, **kwargs):
+class RemoteShell:
 
-        self.table = networktables.NetworkTables.getTable('RemoteREPL')
-        self.interpreter = code.InteractiveInterpreter(locals={"robot": robot, "r": robot})
-        self.table.putString("stdin", "")
-        self.table.putString("stdout", "")
+	def __init__(self, robot):
 
-        def ep(entry, *args, **kwargs):
-            stdout = io.StringIO()
-            with redirect_stdout(stdout):
-                stderr = io.StringIO()
-                with redirect_stderr(stderr):
-                    self.interpreter.runsource(entry.value.getRaw()[:-22])
+		self.inst = ntcore.NetworkTableInstance.getDefault()
+		self.table = self.inst.getTable('Remote Shell')
+		self.interpreter = code.InteractiveInterpreter(locals={"robot": robot, "r": robot})
+		# self.table.putString("stdin", "")
+		self.stdin_sub = self.table.getStringTopic("stdin").subscribe("")
+		self.table.putString("stdout", "")
 
-            out = stdout.getvalue()
+		def ep(event: ntcore.Event, *args, **kwargs):
+			# print("got", entry.value.getRaw())
 
-            if not out.strip():
-                out = "\n"
+			stdout = io.StringIO()
+			with redirect_stdout(stdout):
+				stderr = io.StringIO()
+				with redirect_stderr(stderr):
+					self.interpreter.runsource(event.data.value.getString()[:-22]) # type: ignore
 
-            out += stderr.getvalue()
+			out = stdout.getvalue()
 
-            out += f" T{time.time_ns():<20}"
+			if not out.strip():
+				out = "\n"
 
-            self.table.putString("stdout", out)
+			out += stderr.getvalue()
 
-        self.table.getEntry("stdin").addListener(ep, networktables.NetworkTablesInstance.NotifyFlags.UPDATE | networktables.NetworkTablesInstance.NotifyFlags.NEW)
+			out += f" T{time.time_ns():<20}"
+
+			self.table.putString("stdout", out)
+
+		self.inst.addListener(self.stdin_sub, ntcore.EventFlags.kValueAll, ep)
+		# self.table.getEntry("stdin").addListener(ep, ntcore.NetworkTablesInstance.NotifyFlags.UPDATE | networktables.NetworkTablesInstance.NotifyFlags.NEW)
